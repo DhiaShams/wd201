92b13d70a92de598f31adef4f7ebdcbbc3f0e894
DhiaShams <dhiadhilu123@gmail.com>
2025-03-25 23:13:24 +0530

    Modified to meet requirements

--- http-server/index.js
+++ http-server/index.js
@@ -1,51 +1,55 @@
 const http = require("http");
 const fs = require("fs");
 const minimist = require("minimist");
+
 const args = minimist(process.argv.slice(2));
+const PORT = args.port || 3000; // Default port to 3000 if not specified
+
 let homeContent = "";
 let projectContent = "";
+let registrationContent = "";
 
-// Read home.html
-fs.readFile("home.html", (err, home) => {
+// Read files before starting server
+fs.readFile("home.html", (err, data) => {
   if (err) {
-    throw err;
+    console.error("Error reading home.html:", err);
+    return;
   }
-  homeContent = home;
+  homeContent = data;
 });
 
-// Read project.html
-fs.readFile("project.html", (err, project) => {
-    if (err) {
-      throw err;
-    }
-    projectContent = project;
-  });
-  
-  // Read registration.html
-  fs.readFile("registration.html", (err, registration) => {
-    if (err) {
-      throw err;
-    }
-    registrationContent = registration;
-  });
+fs.readFile("project.html", (err, data) => {
+  if (err) {
+    console.error("Error reading project.html:", err);
+    return;
+  }
+  projectContent = data;
+});
 
-    // Start server only after reading both files
-    http.createServer((request, response) => {
-      let url = request.url;
-      response.writeHead(200, { "Content-Type": "text/html" });
+fs.readFile("registration.html", (err, data) => {
+  if (err) {
+    console.error("Error reading registration.html:", err);
+    return;
+  }
+  registrationContent = data;
+});
 
-      switch (url) {
-        case "/project":
-          response.write(projectContent);
-          break;
-        case "/registration":
-          response.write(registrationContent);
-          break;
-        default:
-          response.write(homeContent);
-          break;
-      }
-      response.end();
-    }).listen(3000, () => {
-      console.log("Server is running at http://localhost:3000/");
-    });
+// Create the server
+const server = http.createServer((request, response) => {
+  let url = request.url;
+  response.writeHead(200, { "Content-Type": "text/html" });
+
+  if (url === "/project") {
+    response.write(projectContent);
+  } else if (url === "/registration") {
+    response.write(registrationContent);
+  } else {
+    response.write(homeContent);
+  }
+  response.end();
+});
+
+// Start the server on the specified port
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});

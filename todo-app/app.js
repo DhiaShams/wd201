f66309548f0495b7952a3c2d4f3976a2068354a6
DhiaShams <dhiadhilu123@gmail.com>
2025-05-11 19:56:20 +0530

    corrected path

--- todo-app/app.js
+++ todo-app/app.js
@@ -84,6 +84,9 @@ passport.deserializeUser((id, done) => {
 const { Todo, User } = require("./models");
 
 app.get("/", async (request, response) => {
+  if (request.isAuthenticated()) {
+    return response.redirect("/todos");
+  }
   response.render("index", {
     title: "Todo Application",
     csrfToken: request.csrfToken(),
